a294eeee6b8b77823e06aee915fbd666813a62c7
FranciscoGJR <franciscoogjr@gmail.com>
2023-10-06 17:10:59 -0300

    feature: implementado @BeforeEach e @BeforeAll em CursoTeste

--- src/test/java/com/banco/curso/CursoTeste.java
+++ src/test/java/com/banco/curso/CursoTeste.java
@@ -3,13 +3,28 @@ package com.banco.curso;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.banco.model.Curso;
 import com.banco.services.CursoServices;
 
+@TestInstance(Lifecycle.PER_CLASS)
 class CursoTeste {
+	
+	@BeforeAll
+	void iniciando(){
+		System.out.println("Classe de teste CursoTeste iniciada");
+	}
+	
+	@BeforeEach
+	void antesDeCadaUm() {
+		System.out.println("Testando um dos métodos");
+	}
 
 	@Autowired
 	CursoServices cursoServices;
